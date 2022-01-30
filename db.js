11104910407a280021cd13c09d0001336750ba5c
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-01-30 23:13:31 +0900

    PW hash 처리를 통해 DB 저장 구현

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql");
 
+
 const DB = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
     database: 'todo_db',
-    port:3306
+    // port:3306
 });
+
 
 DB.connect(function(err){
     if (err) throw err;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const DB = require('./db');
+const bcrypt = require('bcrypt-nodejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,16 +20,33 @@ app.listen(PORT, () => {
 
 //post API
 
+
 //회원가입 (DB연동 후 API를 통해 유저 정보 DB로 전송)
-app.post('/register', (req, res) => {
-    const param = [req.body.birth, req.body.name, req.body.password];
+//bcrpt를 통해 password를 hash처리하여 DB로 전송
 
-    DB.query('INSERT INTO users(birth,name,password) VALUES(?,?,?);', param, (err, result) => {
+app.post('/register', async(req, res) => {
 
-        console.log(param);
+    const BIRTH = req.body.birth;
+    const NAME = req.body.name;
+    const PASSWORD = req.body.password;
+    
+    await bcrypt.hash (PASSWORD, null, null, (err, hash) => {
+        const sql = 'INSERT INTO users(birth,name,password) VALUES(?,?,?);';
+        const param = [BIRTH, NAME, hash];
+        DB.query(sql, param, (err, result) => {
+            if (err){
+                console.log(err);
+                res.status(500).send('ERROR');
+            }else{
+            console.log('success sign-up!');
+            console.log('hash');
+            }
+            res.end();
+        })
 
     });
-    res.end();
+
+
 });
 
 
