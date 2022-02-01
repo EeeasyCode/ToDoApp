497912a9e91b05fbe42d017214ed8d1e53c09ea8
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-02-01 21:36:54 +0900

    회원가입 ID 중복 인증 로직 구현

--- server.js
+++ server.js
@@ -17,41 +17,65 @@ app.listen(PORT, () => {
 });
  
 
+//GET API
+// app.get('/login')
 
-//post API
 
 
-//회원가입 (DB연동 후 API를 통해 유저 정보 DB로 전송)
+
+app.get('/register', (req, res) => {
+    console.log('회원가입 페이지');
+    res.render('register');
+});
+
+
+//회원가입 API(DB연동 후 API를 통해 유저 정보 DB로 전송)
 //bcrpt를 통해 password를 hash처리하여 DB로 전송
 
 app.post('/register', async(req, res) => {
+    console.log('회원가입 중');
 
+    const ID = req.body.user_id;
+    const EMAIL = req.body.email;
+    const PHONE_NUM = req.body.phone;
     const BIRTH = req.body.birth;
     const NAME = req.body.name;
     const PASSWORD = req.body.password;
     
     await bcrypt.hash (PASSWORD, null, null, (err, hash) => {
-        const sql = 'INSERT INTO users(birth,name,password) VALUES(?,?,?);';
-        const param = [BIRTH, NAME, hash];
-        DB.query(sql, param, (err, result) => {
-            if (err){
-                console.log(err);
-                res.status(500).send('ERROR');
+        const sql = 'INSERT INTO users(user_id,email,phone,birth,name,password) VALUES(?,?,?,?,?,?);';
+        const param = [ID, EMAIL, PHONE_NUM, BIRTH, NAME, hash];
+        DB.query('SELECT * FROM users WHERE user_id=?',param[0],(err, result) => {
+            if(err) console.log(err);
+
+            if(result.length> 0){
+                res.send('이미 사용중인 ID입니다.');
             }else{
-            console.log('success sign-up!');
-            console.log('hash');
-            }
-            res.end();
-        })
+                DB.query(sql, param, (err, result) => {
+                    if (err){
+                        console.log(err);
+                        res.status(500).send('ERROR');
+                    }else{
+                    console.log('success sign-up!');
+                    }
+                    res.end();
+                });
+            };
 
+        });
+       
     });
-
-
 });
 
 
-// //로그인
-// app.post('/login')
+// 로그인 API
+app.post('/login', () => {
+   const ID = req.body.id; 
+   const PASSWORD = req.body.password;
+   
+   const sql = '';
+   
+});
 
 
 // //로그인 이후 모든 todo-list 가져오기
