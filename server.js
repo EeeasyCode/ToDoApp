4d1ab6042caff6d93e131d672e620aba9f26e1c2
changmin Lee / 이창민 <92606611+EeeasyCode@users.noreply.github.com>
2022-02-15 10:43:53 +0900

    라우팅 수정

--- server.js
+++ server.js
@@ -3,9 +3,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const DB = require('./config/db');
-const crypto = require('crypto');
-const { rejects } = require('assert');
+// const DB = require('./config/db');
+// const crypto = require('crypto');
 const user = require('./usercontroller');
 
 require('dotenv').config();
@@ -36,71 +35,17 @@ app.get('/register', (req, res) => {
 //회원가입 API(DB연동 후 API를 통해 유저 정보 DB로 전송)
 //crypto를 통해 password를 hash처리하여 DB로 전송
 
-app.post('/register', async(req, res) => {
-    console.log('회원가입 중');
-
-    const ID = req.body.user_id;
-    const EMAIL = req.body.email;
-    const PHONE_NUM = req.body.phone;
-    const BIRTH = req.body.birth;
-    const NAME = req.body.name;
-    const PASSWORD = req.body.password;
-        
-    //salt 생성
-    const createSalt = () =>
-        new Promise((resolve, reject) => {
-            crypto.randomBytes(64, (err, buf) => {
-                if (err) reject(err);
-                resolve(buf.toString('base64'));
-            });
-        });
-    
-    //해시값 생성
-    const createHashedPassword = (plainPassword) =>
-        new Promise(async (resolve, reject) => {
-            const salt = await createSalt();
-            crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
-                if (err) reject(err);
-                resolve({ password: key.toString('base64'), salt});
-            });
-        });
-
-    const { password, salt } = await createHashedPassword(PASSWORD);
-
-    //회원가입 DB 연동 로직
-    const param = [ID, EMAIL, PHONE_NUM, BIRTH, NAME, password, salt];
-    DB.query('SELECT * FROM users WHERE user_id=?',param[0],(err, result) => {
-        if(err) console.log(err);
-
-        if(result.length> 0){
-            res.send('이미 사용중인 ID입니다.');
-        }else{
-            const sql = 'INSERT INTO users(user_id,email,phone,birth,name,password,salt) VALUES(?,?,?,?,?,?,?);';
-            DB.query(sql, param, (err, result) => {
-                if (err){
-                    console.log(err);
-                    res.status(500).send('ERROR');
-                }else{
-                console.log('success sign-up!');
-                }
-                res.end();
-            });
-        };
-
-    });
-    
-
-});
+app.post('/register', user.register);
 
 //회원가입 시 문자인증(SENS를 통한) 전송 API
-app.post('/register/send', user.send);
+app.post('/sms/send', user.send);
 
 //회원가입 시 문자인증(SENS를 통한) 검증 API
-app.get('/register/verify', user.verify);
+app.get('/sms/verify', user.verify);
 
 
 
-
+//oauth
 // 로그인 API
 // app.post('/login', () => {
 //    const ID = req.body.id; 
--- usercontroller.js
+++ usercontroller.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const Cache = require('memory-cache');
 const CryptoJS = require('crypto-js');
 
+const DB = require('./config/db');
+const crypto = require('crypto');
+
 const date = Date.now().toString();
 const uri = secret_key.NCP_serviceID;
 const secretKey = secret_key.NCP_secretKey;
@@ -84,9 +87,64 @@ exports.verify = async function (req, res) {
         return res.end();
     } else {
       Cache.del(phoneNumber);
-      return res.send('successverify');     
+      return res.send('verify success');     
     }
 };
 
+exports.register = async(req, res) => {
+    console.log('회원가입 중');
+
+    const ID = req.body.user_id;
+    const EMAIL = req.body.email;
+    const PHONE_NUM = req.body.phone;
+    const BIRTH = req.body.birth;
+    const NAME = req.body.name;
+    const PASSWORD = req.body.password;
+        
+    //salt 생성
+    const createSalt = () =>
+        new Promise((resolve, reject) => {
+            crypto.randomBytes(64, (err, buf) => {
+                if (err) reject(err);
+                resolve(buf.toString('base64'));
+            });
+        });
+    
+    //해시값 생성   
+    const createHashedPassword = (plainPassword) =>
+        new Promise(async (resolve, reject) => {
+            const salt = await createSalt();
+            crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
+                if (err) reject(err);
+                resolve({ password: key.toString('base64'), salt});
+            });
+        });
+
+    const { password, salt } = await createHashedPassword(PASSWORD);
+
+    //회원가입 DB 연동 로직
+    const param = [ID, EMAIL, PHONE_NUM, BIRTH, NAME, password, salt];
+    DB.query('SELECT * FROM users WHERE user_id=?',param[0],(err, result) => {
+        if(err) console.log(err);
+
+        if(result.length> 0){
+            res.send('이미 사용중인 ID입니다.');
+        }else{
+            const sql = 'INSERT INTO users(user_id,email,phone,birth,name,password,salt) VALUES(?,?,?,?,?,?,?);';
+            DB.query(sql, param, (err, result) => {
+                if (err){
+                    console.log(err);
+                    res.status(500).send('ERROR');
+                }else{
+                console.log('success sign-up!');
+                }
+                res.end();
+            });
+        };
+
+    });
+    
+
+}
 //다른 api들도 전부 모듈화, 라우터로 구성
 
